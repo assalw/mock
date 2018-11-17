188aef4266eaf4d99d701da1a770ef6c00d8ca75
Wadie Assal <wadie.assal@true.nl>
2018-11-17 04:44:42 +0100

    Added new activity

--- app/src/main/java/theoutliers/mock/MainActivity.java
+++ app/src/main/java/theoutliers/mock/MainActivity.java
@@ -10,9 +10,12 @@ import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+
+import theoutliers.mock.stages.JourneyActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -63,5 +66,11 @@ public class MainActivity extends AppCompatActivity {
         journey.constructJourney();
 
         // TODO: Test travel subtimes
+    }
+
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, JourneyActivity.class);
+        startActivity(intent);
     }
 }
