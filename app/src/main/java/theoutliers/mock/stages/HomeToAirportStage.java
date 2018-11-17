9c7a4b298248cb84ba4e43b0afd3573646ee2d00
Wadie Assal <wadie.assal@true.nl>
2018-11-17 01:32:23 +0100

    Some changes

--- app/src/main/java/theoutliers/mock/stages/HomeToAirportStage.java
+++ app/src/main/java/theoutliers/mock/stages/HomeToAirportStage.java
@@ -1,11 +1,19 @@
 package theoutliers.mock.stages;
 
+import android.location.LocationManager;
+
 import theoutliers.mock.Config;
 import theoutliers.mock.R;
 
 public class HomeToAirportStage extends Stage {
     
+    public HomeToAirportStage() {
+
+    }
+
     @Override
     public void update(){
+
+
     }
 }
