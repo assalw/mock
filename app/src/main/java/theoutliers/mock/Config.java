1c041580e9fc21e81569491e2e0dbb2cf7313f96
Wadie Assal <wadie.assal@true.nl>
2018-11-17 01:28:38 +0100

    Config class

---
+++ app/src/main/java/theoutliers/mock/Config.java
@@ -0,0 +1,11 @@
+package theoutliers.mock;
+
+public class Config {
+
+    public static String TOMTOM_API_KEY = "skjfhjhds";
+
+    public static String SCHIPHOL_API_KEY = "skjfhjhds";
+
+    public static String GOOGLE_MAPS_API_KEY = "skjfhjhds";
+
+}
--- app/src/main/java/theoutliers/mock/stages/HomeToAirportStage.java
+++ app/src/main/java/theoutliers/mock/stages/HomeToAirportStage.java
@@ -1,9 +1,10 @@
 package theoutliers.mock.stages;
 
+import theoutliers.mock.Config;
 import theoutliers.mock.R;
 
 public class HomeToAirportStage extends Stage {
-
+    
     @Override
     public void update(){
     }
